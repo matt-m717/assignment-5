c53f7ab51b9e8a295407f881673fc056b4e2e44e
matt <nonconservative_vsgas@8shield.net>
2026-02-06 16:08:08 -0500

    Remove BrowserRouter

--- src/pages/___tests___/HomePage.test.jsx
+++ src/pages/___tests___/HomePage.test.jsx
@@ -1,21 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import HomePage from "../HomePage";
 import { describe, expect, test } from "vitest";
 
 const storeName = "ComponentCorner";
 
-const renderWithRouter = component => {
-    return render(<BrowserRouter>{component}</BrowserRouter>);
-};
-
 describe("HomePage", () => {
     test("renders without crashing", () => {
-        renderWithRouter(<HomePage storeName={storeName}></HomePage>);
+        render(<HomePage storeName={storeName}></HomePage>);
     });
 
     test("displays main content", () => {
-        renderWithRouter(<HomePage storeName={storeName}></HomePage>);
+        render(<HomePage storeName={storeName}></HomePage>);
 
         expect(screen.getByText("Why Shop With Us?")).toBeInTheDocument();
         expect(screen.getByText("Quality Products")).toBeInTheDocument();
